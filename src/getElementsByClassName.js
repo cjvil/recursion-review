72b7ae4168b3c0f89b69d3c23e7ae9b71ef5b604
cjvil <christiejvillanueva@gmail.com>
2017-08-21 12:23:42 -0700

    Complete getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,41 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  let returnValues = []
+
+  var checkName = function(name) {
+    if(name === undefined) {
+      return false
+    }
+    let names = name.split(' ')
+    for(let i = 0; i < names.length; i++) {
+      if(names[i] === className) {
+        return true
+      }
+    }
+    return false
+  }
+
+  var recursion = function(element) {
+
+    if(checkName(element.className)) {
+        returnValues.push(element)
+    }
+    if (element.childNodes != undefined) {
+      for(let i = 0; i < element.childNodes.length; i++) {
+        let children = element.childNodes[i]
+        if (children.childNodes != undefined) {
+          recursion(children)
+        } else if(checkName(element.className)) {
+          returnValues.push(children)
+        }
+      }
+    }
+    
+  }
+
+  let elements = document.body
+  recursion(elements)
+  return returnValues
+
 };
