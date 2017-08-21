a48a22e14886d86f40ee0d688726c9452e65df1f
cjvil <christiejvillanueva@gmail.com>
2017-08-21 11:36:04 -0700

    Complete stringifyJSON and debug tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,52 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(obj === null) {
+    return 'null';
+  }
+
+  if(typeof obj === 'function' ||typeof obj === 'undefined') {
+    return '';
+  }
+
+  var output = '';
+  
+  output += obj;
+  
+  if(typeof obj === 'string') {
+    return '"' + obj + '"';
+  } 
+
+  if(Array.isArray(obj)) {
+    if(obj.length === 0) {
+      return '[]';
+    }
+    
+    let array = [];
+
+    for(let i = 0; i < obj.length; i++) {
+      array.push(stringifyJSON(obj[i]));
+    }
+    
+    return '[' + array.join(',') + ']'
+  }
+
+  if (typeof obj === 'object') {
+    if(obj.length === 0) {
+      return '{}';
+    }
+    
+    let object = [];
+
+    for(var key in obj) {
+      let stringifyObj = stringifyJSON(obj[key])
+      if(stringifyObj != '') {
+        object.push('"' + key + '"' + ':' + stringifyObj);
+      }
+    }
+
+    return '{' + object.join(',') + '}';
+  }
+
+  return output;
 };
